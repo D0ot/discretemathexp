5912b01f48e9200deb8059b3062be59f072d1f30
D0ot <gbcays@qq.com>
2019-10-11 18:55:33 +0800

    first commit

---
+++ build/CMakeFiles/discretemath.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/discretemath.dir/exp2.cpp.o"
+  "discretemath.pdb"
+  "discretemath"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/discretemath.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/doot/learnthings/discretemath
+# Build directory: /home/doot/learnthings/discretemath/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
