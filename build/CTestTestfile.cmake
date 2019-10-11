b47028d3f3a03f22df4c8815ac6b246018ca39c7
D0ot <gbcays@qq.com>
2019-10-11 22:24:25 +0800

    clean commit

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/doot/learnthings/discretemath
-# Build directory: /home/doot/learnthings/discretemath/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
